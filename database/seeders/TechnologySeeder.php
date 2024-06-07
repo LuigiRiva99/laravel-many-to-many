1b934dfbe6e0ec293500780b754e6c19d9a405d8
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-06-07 15:54:22 +0200

    ADD: populated technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['CSS', 'JS', 'SQL', 'VUE', 'PHP', 'LARAVEL'];
+
+        foreach ($technologies as $tec_name) {
+            $new_tec = new Technology();
+            $new_tec->name = $tec_name;
+
+            $new_tec->save();
+
+        }
     }
 }
